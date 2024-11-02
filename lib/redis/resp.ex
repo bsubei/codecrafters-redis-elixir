d6afae4b0b33892a6c89adc8ad30cb368ee1d152
Basheer Subei <6508762+bsubei@users.noreply.github.com>
2024-11-02 15:54:34 +0000

    support decoding nested arrays with a one line change. I love Elixir! Also, add some more type specs

--- lib/redis/resp.ex
+++ lib/redis/resp.ex
@@ -14,9 +14,13 @@ defmodule Redis.RESP do
   def crlf, do: @crlf
   @crlf_iodata [?\r, ?\n]
 
+  @type encoding_t :: :integer | :simple_string | :bulk_string | :simple_error | :array
+
   @doc ~S"""
     Encodes a list of Elixir terms as an iodata of a Redis (RESP) Array of Bulk Strings.
     Alternatively, if a single element is provided, the output will be encoded as a simple string.
+
+    NOTE: encoding of nested arrays is not supported, although decoding is.
 
     ## Examples
 
@@ -46,7 +50,7 @@ defmodule Redis.RESP do
         iex> IO.iodata_to_binary(Redis.RESP.encode("ErRoR: WHAT DID YOU DO?!", :simple_error))
         "-ErRoR: WHAT DID YOU DO?!\r\n"
   """
-  @spec encode([String.Chars.t()] | String.Chars.t(), atom()) :: iodata
+  @spec encode([String.Chars.t()] | String.Chars.t(), encoding_t()) :: iodata
   def encode(input, encoding_type)
 
   def encode(input, :integer) when is_binary(input),
@@ -81,6 +85,9 @@ defmodule Redis.RESP do
   @spec make_array([String.Chars.t()]) :: String.Chars.t()
   def make_array(input), do: IO.iodata_to_binary(encode(input, :array))
 
+  @type resp_value_t :: binary() | list(resp_value_t())
+  @type decoding_result_t(value_t) :: {:ok, value_t, binary()} | :error
+
   @doc ~S"""
   Decodes a RESP-encoded value from the given `data`.
 
@@ -99,22 +106,36 @@ defmodule Redis.RESP do
       iex> Redis.RESP.decode("$3\r\n\0hi\r\n")
       {:ok, <<0, "hi">>, ""}
 
+      iex> Redis.RESP.decode("$4\r\nPONG\r\n")
+      {:ok, "PONG", ""}
+
       iex> Redis.RESP.decode("*2\r\n$2\r\nOK\r\n$3\r\nbye\r\nleftovercrud")
       {:ok, ["OK", "bye"], "leftovercrud"}
 
       iex> Redis.RESP.decode("-ERR please don't do that\r\nunused")
       {:ok, "ERR please don't do that", "unused"}
 
+      iex> Redis.RESP.decode("+:+1:\r\n")
+      {:ok, ":+1:", ""}
 
+      iex> Redis.RESP.decode("$4\r\nmore than four bytes\r\n")
+      :error
+
+      iex> Redis.RESP.decode(":--1\r\n")
+      :error
+
+      iex> Redis.RESP.decode(":-1\r\n")
+      {:ok, -1, ""}
   """
-  @spec decode(binary()) :: {:ok, list(binary()) | binary(), binary()} | :error
-  def decode(input) when is_binary(input), do: decode_impl(input)
-  defp decode_impl("*" <> rest), do: decode_array(rest)
-  defp decode_impl("+" <> rest), do: decode_simple_string(rest)
+  @spec decode(binary()) :: decoding_result_t(resp_value_t())
+  def decode("*" <> rest), do: decode_array(rest)
+  def decode("+" <> rest), do: decode_simple_string(rest)
   # NOTE: decoding simple errors is the same as simple strings once we strip off the "-" prefix.
-  defp decode_impl("-" <> rest), do: decode_simple_string(rest)
-  defp decode_impl("$" <> rest), do: decode_bulk_string(rest)
+  def decode("-" <> rest), do: decode_simple_string(rest)
+  def decode("$" <> rest), do: decode_bulk_string(rest)
+  def decode(":" <> rest), do: decode_integer(rest)
 
+  @spec decode_array(binary()) :: decoding_result_t(list(resp_value_t()))
   defp decode_array(input) do
     # First, grab the array length.
     case decode_positive_integer(input) do
@@ -123,42 +144,53 @@ defmodule Redis.RESP do
     end
   end
 
+  @spec decode_array_impl(binary(), integer(), list(resp_value_t())) ::
+          decoding_result_t(list(resp_value_t()))
   defp decode_array_impl(data, count_remaining, accumulator \\ [])
 
   defp decode_array_impl(rest, 0, accumulator), do: {:ok, accumulator, rest}
 
-  defp decode_array_impl(<<?$, data::binary>>, count_remaining, accumulator) do
-    {:ok, this_elem, rest} = decode_bulk_string(data)
-    decode_array_impl(rest, count_remaining - 1, accumulator ++ [this_elem])
+  defp decode_array_impl(data, count_remaining, accumulator) do
+    case decode(data) do
+      :error ->
+        :error
+
+      {:ok, parsed_data, rest} ->
+        decode_array_impl(rest, count_remaining - 1, accumulator ++ [parsed_data])
+    end
   end
 
-  defp decode_array_impl(_rest, _count_remaining, _accumulator), do: :error
-
-  # Recursively call decode_integer, skimming off the leftmost digit each time and accumulating all of them until we have the final number after hitting a crlf.
+  # Recursively call decode_positive_integer, skimming off the leftmost digit each time and accumulating all of them until we have the final number after hitting a crlf.
+  @spec decode_positive_integer(binary(), integer()) :: decoding_result_t(integer())
   def decode_positive_integer(data, accumulator \\ 0)
 
-  def decode_positive_integer(<<@crlf, rest::binary>>, accumulator), do: {:ok, accumulator, rest}
+  def decode_positive_integer(@crlf <> rest, accumulator), do: {:ok, accumulator, rest}
 
   def decode_positive_integer(<<digit, rest::binary>>, accumulator) when digit in ?0..?9,
     do: decode_positive_integer(rest, accumulator * 10 + (digit - ?0))
 
   def decode_positive_integer(_data, _accumulator), do: :error
 
+  @spec decode_integer(binary(), integer()) :: decoding_result_t(integer())
   def decode_integer(data, accumulator \\ 0)
 
-  # If the integer starts with a negative sign, then decode the rest as a positive and slap the negative on in the end.
+  # If the integer starts with a negative sign, then decode the rest as we normally do for positive ints and slap the negative on in the end.
   def decode_integer(<<?-, rest::binary>>, accumulator) do
-    {:ok, accumulator, rest} = decode_positive_integer(rest, accumulator)
-    {:ok, -accumulator, rest}
+    case decode_positive_integer(rest, accumulator) do
+      {:ok, accumulator, rest} -> {:ok, -accumulator, rest}
+      :error -> :error
+    end
   end
 
   # Otherwise, decode it as a positive integer normally.
   def decode_integer(data, accumulator), do: decode_positive_integer(data, accumulator)
 
   # To decode a simple string, just keep grabbing bytes until you run into crlf.
+  @spec decode_simple_string(binary()) :: decoding_result_t(binary())
   defp decode_simple_string(input), do: until_crlf(input)
 
   # Return all the bytes until the crlf.
+  @spec until_crlf(binary(), binary()) :: decoding_result_t(binary())
   defp until_crlf(input, accumulator \\ "")
 
   defp until_crlf(<<@crlf, rest::binary>>, accumulator), do: {:ok, accumulator, rest}
@@ -169,11 +201,14 @@ defmodule Redis.RESP do
 
   defp until_crlf(<<>>, _accumulator), do: :error
 
+  @spec decode_bulk_string(binary()) :: decoding_result_t(binary())
   defp decode_bulk_string(input) do
     case decode_positive_integer(input) do
-      # TODO use the actual length when parsing.
-      {:ok, _length, rest} ->
-        until_crlf(rest)
+      {:ok, num_bytes, rest_to_decode} ->
+        case rest_to_decode do
+          <<bytes::binary-size(num_bytes), @crlf, rest::binary>> -> {:ok, bytes, rest}
+          _ -> :error
+        end
 
       :error ->
         :error
--- test/redis/resp_test.exs
+++ test/redis/resp_test.exs
@@ -18,5 +18,22 @@ defmodule Redis.RESPTest do
       assert {:ok, ["GET", "THE_KEY"], ""} ==
                RESP.decode(IO.iodata_to_binary(RESP.encode(["GET", "THE_KEY"], :array)))
     end
+
+    test "decoding nested RESP arrays" do
+      # Decoding a nested array of arrays of simple strings.
+      assert {:ok, [["0,0", "0,1"], ["1,0", "1,1"]], "unused"} =
+               Redis.RESP.decode("*2\r\n*2\r\n+0,0\r\n+0,1\r\n*2\r\n+1,0\r\n+1,1\r\nunused")
+
+      # Decoding the nested array example from the codecrafters (this is the kind of responses our server should generate for XRANGE stream requests).
+      input =
+        "*2\r\n*2\r\n$15\r\n1526985054069-0\r\n*4\r\n$11\r\ntemperature\r\n$2\r\n36\r\n$8\r\nhumidity\r\n$2\r\n95\r\n*2\r\n$15\r\n1526985054079-0\r\n*4\r\n$11\r\ntemperature\r\n$2\r\n37\r\n$8\r\nhumidity\r\n$2\r\n94\r\n"
+
+      expected_output = [
+        ["1526985054069-0", ["temperature", "36", "humidity", "95"]],
+        ["1526985054079-0", ["temperature", "37", "humidity", "94"]]
+      ]
+
+      assert {:ok, ^expected_output, ""} = Redis.RESP.decode(input)
+    end
   end
 end
